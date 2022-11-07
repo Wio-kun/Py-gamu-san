f4f02f99ee229863882def123b075f5383d14cb0
Wio-kun <edvval09@vgs.nfk.no>
2022-11-07 15:31:04 +0100

    2

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ PURPLE = (175,150,225)
 
 screen = pg.display.set_mode((1000,1000))
 player_img = pg.image.load("black_knight.png")
+player_img = pg.transform.scale(player_img, (160,256))
 
 fps_counter = 0
 x = 0
@@ -42,18 +43,16 @@ while playing:
     if keys [pg.K_d]:
         x += speed
 
-    if x > 1000:
-        x = 1000
-    if x < -100:
-        x = -100
-    if y > 1000:
-        y = 1000
-    if y < -100:
-        y = -100
-
-    box = pg.Rect(x,y, 100,100)
-    pg.draw.rect(screen, PURPLE, box)
+    if x > 1160:
+        x = 1160
+    if x < -160:
+        x = -160
+    if y > 1256:
+        y = 256
+    if y < -744:
+        y = -744
 
     screen.blit(player_img, (x,y))
+
 
     pg.display.update()
