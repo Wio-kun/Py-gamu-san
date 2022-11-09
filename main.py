64711684dd19250b75dae63ccfe809ad1b9d5447
Wio-kun <edvval09@vgs.nfk.no>
2022-11-09 13:41:50 +0100

    pc update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from sprites import *
 
 pg.init()
 
@@ -8,20 +9,15 @@ GRØNN = (77,168,25)
 MINT = (150,200,175)
 PURPLE = (175,150,225)
 
+all_sprites = pg.sprite.Group()
+
+Knight = Player()
+all_sprites.add(Knight)
+
 screen = pg.display.set_mode((1000,1000))
-player_img = pg.image.load("black_knight.png")
-player_img = pg.transform.scale(player_img, (160,256))
 
-fps_counter = 0
-x = 0
-y = 0
-
-speed = 5
 FPS = 120
 clock = pg.time.Clock()
-
-direction_x = + 1
-direction_y = + 1
 
 playing = True
 while playing:
@@ -32,27 +28,8 @@ while playing:
 
     screen.fill(MINT)
 
-    keys = pg.key.get_pressed()
+    all_sprites.update()
 
-    if keys [pg.K_w]:
-        y -= speed
-    if keys [pg.K_a]:
-        x -= speed
-    if keys [pg.K_s]:
-        y += speed
-    if keys [pg.K_d]:
-        x += speed
-
-    if x > 1160:
-        x = 1160
-    if x < -160:
-        x = -160
-    if y > 1256:
-        y = 256
-    if y < -744:
-        y = -744
-
-    screen.blit(player_img, (x,y))
-
+    all_sprites.draw(screen)
 
     pg.display.update()
---
+++ sprites.py
@@ -0,0 +1,27 @@
+import pygame as pg
+vec = pg.math.Vector2
+
+player_img = pg.image.load("knight.png")
+player_img = pg.transform.scale(player_img, (120,165))
+
+class Player(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.image = player_img
+        self.rect = self.image.get_rect()
+        self.pos = vec(100, 100)
+        self.rect.center = self.pos
+        self.speed = 3
+
+    def update(self):
+        self.rect.center = self.pos
+
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w]:
+            self.pos.y -= 3
+        if keys[pg.K_s]:
+            self.pos.y += 3
+        if keys[pg.K_a]:
+            self.pos.x -= 3
+        if keys[pg.K_d]:
+            self.pos.x += 3
