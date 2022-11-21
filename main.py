cfdb90d156966482795b68fd143ef0a83722cad2
Wio-kun <edvval09@vgs.nfk.no>
2022-11-21 15:27:43 +0100

    bn

--- main.py
+++ main.py
@@ -8,26 +8,30 @@ BLACK = (0,0,0)
 GREEN = (77,168,25)
 MINT = (150,200,175)
 PURPLE = (175,150,225)
+WIDTH = 1216
+HEIGHT = 1238
 
 all_sprites = pg.sprite.Group()
 boss_group = pg.sprite.Group()
 knight_group = pg.sprite.Group()
-bg_group = pg.sprite.Group()
 
 Knight = Player()
 Black_Knight = Boss()
 all_sprites.add(Knight, Black_Knight)
 boss_group.add(Black_Knight)
 knight_group.add(Knight)
-Bg = Background()
-bg_group.add(bg_image)
 
 
-screen = pg.display.set_mode((1440,720))
+screen = pg.display.set_mode((WIDTH,HEIGHT))
+bg=pg.image.load("floor 1.png").convert_alpha()
+
+bg=pg.transform.scale(bg, (WIDTH, HEIGHT))
 
 
 FPS = 120
 clock = pg.time.Clock()
+
+i=0
 
 playing = True
 while playing:
@@ -36,8 +40,9 @@ while playing:
         if event.type == pg.QUIT:
             playing = False
 
-    screen.fill(MINT)
-    screen.blit(Bg.image)
+    screen.blit(bg, (i,0))
+    screen.blit(bg,(WIDTH+i,0))
+
 
     hits = pg.sprite.spritecollide(Black_Knight, knight_group, True)
 
--- sprites.py
+++ sprites.py
@@ -2,20 +2,21 @@ import pygame as pg
 vec = pg.math.Vector2
 
 player_img = pg.image.load("knight.png")
-player_img = pg.transform.scale(player_img, (120,165))
+player_img = pg.transform.scale(player_img, (76,148))
 boss_image = pg.image.load("black_knight.png")
-boss_image = pg.transform.scale(boss_image, (148, 220))
-bg_image = pg.image.load("background.png")
-bg_image = pg.transform.scale(bg_image, (1440, 720))
+boss_image = pg.transform.scale(boss_image, (120, 208))
+player_img_180 = pg.transform.flip(player_img, True, False)
 
 class Player(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
         self.image = player_img
         self.rect = self.image.get_rect()
+        self.image_180 = player_img_180
         self.pos = vec(500, 500)
         self.rect.center = self.pos
         self.speed = 3
+
 
     def update(self):
         self.rect.center = self.pos
@@ -27,19 +28,21 @@ class Player(pg.sprite.Sprite):
             self.pos.y += self.speed
         if keys[pg.K_a]:
             self.pos.x -= self.speed
+            self.image = player_img_180
         if keys[pg.K_d]:
             self.pos.x += self.speed
+            self.image = player_img
 
 class Boss(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
         self.image = boss_image
         self.rect = self.image.get_rect()
-        self.pos = vec(1325, 610)
+        self.pos = vec(1156, 155)
         self.rect.center = self.pos
         self.speed = 2
-        self.direction_x = -5
-        self.direction_y = 0
+        self.direction_x = 0
+        self.direction_y = 5
     
     def update(self):
         self.rect.center = self.pos
@@ -47,34 +50,29 @@ class Boss(pg.sprite.Sprite):
         self.pos.x += self.direction_x
         self.pos.y += self.direction_y
 
-        if self.pos.x < 75:
+        if self.pos.x < 60:
             self.direction_x = 0
             self.direction_y = -5
-            self.pos.x = 75
+            self.pos.x = 60
 
-        if self.pos == vec(75, 605):
+        if self.pos == vec(60, 1095):
             self.image = pg.transform.flip(self.image, True, False)
 
-        if self.pos.y < 115:
+        if self.pos.y < 155:
             self.direction_x = 5
             self.direction_y = 0
-            self.pos.y = 115
+            self.pos.y = 155
 
-        if self.pos == vec(1325, 120):
+        if self.pos == vec(1156, 160):
             self.image = pg.transform.flip(self.image, True, False)
 
-        if self.pos.x > 1325:
+        if self.pos.x > 1156:
             self.direction_x = 0
             self.direction_y = 5
-            self.pos.x = 1325
+            self.pos.x = 1156
 
-        if self.pos.y > 610:
+        if self.pos.y > 1100:
             self.direction_x = -5
             self.direction_y = 0
-            self.pos.y = 610
+            self.pos.y = 1100
 
-class Background(pg.sprite.Sprite):
-    def __init__(self):
-        pg.sprite.Sprite.__init__(self)  #call Sprite initializer
-        
-        
